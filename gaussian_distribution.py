51010ea283c003e7d99407dce42ab2ee931e9210
Babak <babakshofficial@yahoo.co.uk>
2019-11-02 22:03:19 +0330

    edit variables

--- gaussian_distribution.py
+++ gaussian_distribution.py
@@ -1,10 +1,10 @@
 import numpy as np
 import math
 
-def d_dim_calculate(x, dim, mean_vector, cov_matrix):
-    P = 1 / (((2 * math.pi) ^ dim/2) * (np.linalg.det(cov_matrix)) ^ 0.5) * math.exp((-1 * 0.5) * np.matrix.transpose(x - mean_vector) * (np.linalg.inv(cov_matrix)) * (x - mean_vector))
+def d_dim_calculate(x, dim, mean, cov_matrix):
+    P = 1 / (((2 * math.pi) ^ dim/2) * (np.linalg.det(cov_matrix)) ^ 0.5) * math.exp((-1 * 0.5) * np.matrix.transpose(x - mean) * (np.linalg.inv(cov_matrix)) * (x - mean))
     return P
 
-def one_dim_calculate(x, mean_vector, variance):
-    P = 1 / math.sqrt(2 * math.pi * variance) * math.exp(-1 * (((x - mean_vector) ** 2) / (2 * variance)))
+def one_dim_calculate(x, mean, variance):
+    P = 1 / math.sqrt(2 * math.pi * variance) * math.exp(-1 * (((x - mean) ** 2) / (2 * variance)))
     return P
