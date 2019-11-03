007157ca0f5b7c4891bbe10b714e5bd1452f2b06
Babak <babakshofficial@yahoo.co.uk>
2019-11-03 19:18:46 +0330

    finishing gaussian dist function

--- gaussian_distribution.py
+++ gaussian_distribution.py
@@ -2,7 +2,15 @@ import numpy as np
 import math
 
 def d_dim_calculate(x, dim, mean, cov_matrix):
-    P = 1 / (((2 * math.pi) ^ dim/2) * (np.linalg.det(cov_matrix)) ^ 0.5) * math.exp((-1 * 0.5) * np.matrix.transpose(x - mean) * (np.linalg.inv(cov_matrix)) * (x - mean))
+    #coef = 1 / (((2 * math.pi) ** dim/2) * (np.linalg.det(cov_matrix)) ** 0.5)
+    # P = 1 * np.exp((-1 * 0.5) *  np.multiply(np.multiply(np.matrix.transpose(np.subtract(x,mean)) ,(np.linalg.inv(cov_matrix))), (np.subtract(x - mean))))
+    coef1 = (2 * math.pi) ** (dim / 2)
+    coef2 = np.linalg.det(cov_matrix) ** (1/2)
+    coef = coef1 * coef2
+    transpose = np.subtract(x,mean).T
+    inv = np.linalg.inv(cov_matrix)
+    sub = np.subtract(x,mean)
+    P = (1/coef) * np.exp( np.dot(transpose,inv,sub))
     return P
 
 def one_dim_calculate(x, mean, variance):
