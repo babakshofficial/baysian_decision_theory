14302b3226161550a0cd217cdfaf08c01802a980
Babak <babakshofficial@yahoo.co.uk>
2019-11-06 12:52:07 +0330

    done implementation

--- class_accuracy.py
+++ class_accuracy.py
@@ -1,7 +1,10 @@
 def calculate_class_accuracy(real, model, class_number):
     length = len(real)
     true_predictions = 0
+    total_classified_class = 0
     for i in range(length):
         if int(real[i]) == int(model[i]) == class_number:
             true_predictions = true_predictions + 1
-    return true_predictions / length
+        if int(real[i]) == class_number:
+            total_classified_class = total_classified_class + 1
+    return true_predictions / total_classified_class
